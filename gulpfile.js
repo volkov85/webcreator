b1ed3d9d2220fc0c5a49cefaec7c5fb1dad17cc3
Artyom <av@inuigaming.com>
2026-04-07 12:33:07 +0500

    Modernize gulp setup and remove build artifacts

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,61 @@
 "use strict";
 
-var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
-var sass = require("gulp-sass");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var csso = require('gulp-csso');
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var del = require("del");
-var htmlmin = require('gulp-htmlmin');
-var uglify = require('gulp-uglify');
-var pipeline = require('readable-stream').pipeline;
+const path = require("node:path");
+const {dest, series, src, watch} = require("gulp");
+const browserSync = require("browser-sync").create();
+const plumber = require("gulp-plumber");
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const posthtml = require("gulp-posthtml");
+const include = require("posthtml-include");
+const htmlmin = require("gulp-htmlmin");
+const {includePaths} = require("scss-resets");
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+let delModulePromise;
+let imageminModulePromise;
+let webpModulePromise;
+
+function loadDel() {
+  delModulePromise ??= import("del");
+  return delModulePromise;
+}
+
+function loadImagemin() {
+  imageminModulePromise ??= import("gulp-imagemin");
+  return imageminModulePromise;
+}
+
+function loadWebp() {
+  webpModulePromise ??= import("gulp-webp");
+  return webpModulePromise;
+}
+
+function css() {
+  return src("source/sass/style.scss")
     .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass({includePaths: require("scss-resets").includePaths}))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      loadPaths: [
+        ...includePaths,
+        path.join(__dirname, "node_modules", "normalize-scss", "sass")
+      ]
+    }).on("error", sass.logError))
     .pipe(postcss([
       autoprefixer()
     ]))
     .pipe(csso())
     .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
-});
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("build/css"))
+    .pipe(browserSync.stream());
+}
 
-gulp.task("server", function () {
-  server.init({
+function server() {
+  browserSync.init({
     server: "build/",
     notify: false,
     open: true,
@@ -43,78 +63,84 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch("source/js/*.js", gulp.series("jscompress", "refresh"));
-});
+  watch("source/sass/**/*.{scss,sass}", css);
+  watch("source/img/icon-*.svg", series(html, refresh));
+  watch("source/*.html", series(html, refresh));
+  watch("source/js/*.js", series(jscompress, refresh));
+}
 
-gulp.task("images", function () {
-  return gulp.src("source/img/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
+async function images() {
+  const imageminModule = await loadImagemin();
+
+  return src("source/img/*.{png,jpg,svg}", {encoding: false})
+    .pipe(imageminModule.default([
+      imageminModule.optipng({optimizationLevel: 3}),
+      imageminModule.mozjpeg({progressive: true}),
+      imageminModule.svgo()
     ]))
-    .pipe(gulp.dest("source/img"));
-});
+    .pipe(dest("source/img", {encoding: false}));
+}
 
-gulp.task("webp", function () {
-  return gulp.src("source/img/*.{png,jpg}")
+async function webpTask() {
+  const {default: webp} = await loadWebp();
+
+  return src("source/img/*.{png,jpg}", {encoding: false})
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
-});
+    .pipe(dest("source/img", {encoding: false}));
+}
 
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
+function html() {
+  return src("source/*.html")
     .pipe(posthtml([
       include()
     ]))
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("build"));
-});
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(dest("build"));
+}
 
-gulp.task("php", function () {
-  return gulp.src("source/*.php")
-    .pipe(gulp.dest("build"));
-});
+function jscompress() {
+  return src("source/js/*.js")
+    .pipe(dest("build/js"));
+}
 
-gulp.task("jscompress", function () {
-  return pipeline(
-      gulp.src("source/js/*.js"),
-      // uglify(),
-      gulp.dest("build/js")
-  );
-});
+function copy() {
+  return src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/*.ico"
+  ], {
+    base: "source",
+    encoding: false
+  })
+    .pipe(dest("build", {encoding: false}));
+}
 
-gulp.task("copy", function () {
-  return gulp.src([
-      "source/fonts/**/*.{woff,woff2}",
-      "source/img/**",
-      "source/*.ico",
-      "source/phpmailer/**"
-    ], {
-      base: "source"
-    })
-    .pipe(gulp.dest("build"));
-});
+async function clean() {
+  const {deleteAsync} = await loadDel();
+  return deleteAsync(["build"]);
+}
 
-gulp.task("clean", function () {
-  return del("build");
-});
-
-gulp.task("build", gulp.series(
-  "clean",
-  "jscompress",
-  "copy",
-  "css",
-  "html",
-  "php"
-));
-
-gulp.task("refresh", function (done) {
-  server.reload();
+function refresh(done) {
+  browserSync.reload();
   done();
-});
+}
 
-gulp.task("start", gulp.series("build", "server"));
+const build = series(
+  clean,
+  jscompress,
+  copy,
+  css,
+  html
+);
+
+exports.build = build;
+exports.clean = clean;
+exports.copy = copy;
+exports.css = css;
+exports.html = html;
+exports.images = images;
+exports.jscompress = jscompress;
+exports.refresh = refresh;
+exports.server = server;
+exports.start = series(build, server);
+exports.webp = webpTask;
--- source/js/script.js
+++ source/js/script.js
@@ -1,122 +1,447 @@
 'use strict';
 
-const portfolioCards = document.querySelectorAll('.portfolio__cards-button');
-const portfolioDarkerOstltd = document.querySelector('.portfolio__darker--ostltd');
-const portfolioDarkerNda = document.querySelector('.portfolio__darker--nda');
-const portfolioDarkerFant = document.querySelector('.portfolio__darker--fantshop');
-const portfolioDarkerRapid = document.querySelector('.portfolio__darker--rapid');
-const portfolioDarkerAe = document.querySelector('.portfolio__darker--ae');
-const portfolioDarkerBook = document.querySelector('.portfolio__darker--book');
-const portfolioButtonClose = document.querySelectorAll('.portfolio__button-close');
-const portfolioDarker = document.querySelectorAll('.portfolio__darker');
+const portfolioData = {
+  nda: {
+    organization: 'National Digital Aggregator',
+    links: [
+      {
+        href: 'https://ndawp.webcreator.top',
+        label: 'ndawp.webcreator.top'
+      }
+    ],
+    role: 'Frontend development',
+    technologies: 'Figma, HTML5, CSS3, JS, PHP, SASS, Gulp, WordPress',
+    description: 'This site was created for a company that distributes popular Russian music on various platforms. The site is fully responsive, correctly displayed in all possible resolutions and has support for Retina and 4k displays.',
+    images: [
+      { href: 'img/portfolio_full_2.jpg', src: 'img/portfolio_maxi_2_1.jpg', width: 230, height: 176 },
+      { href: 'img/portfolio_full_2.jpg', src: 'img/portfolio_maxi_2_2.jpg', width: 230, height: 215 },
+      { href: 'img/portfolio_full_2.jpg', src: 'img/portfolio_maxi_2_3.jpg', width: 230, height: 175 },
+      { href: 'img/portfolio_full_2.jpg', src: 'img/portfolio_maxi_2_4.jpg', width: 230, height: 188 }
+    ]
+  },
+  fantshop: {
+    organization: 'Fanttex Company',
+    links: [
+      {
+        href: 'https://fantshop.webcreator.top',
+        label: 'fantshop.webcreator.top'
+      },
+      {
+        href: 'https://fantshop.webcreator.top/index-mobile.html',
+        label: 'fantshop.webcreator.top - mobile'
+      }
+    ],
+    role: 'Frontend development',
+    technologies: 'Photoshop, HTML5, CSS3, JS, Pug, SASS, Gulp, JS Libraries',
+    description: 'This site is for a company that is engaged in the wholesale of luxury fabrics. The site is fully responsive, correctly displayed at any resolution and has a separate mobile version. Much attention was paid to optimization, validation in PageSpeed Insights and micro-markup.',
+    images: [
+      { href: 'img/portfolio_full_3.jpg', src: 'img/portfolio_maxi_3_1.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_3.jpg', src: 'img/portfolio_maxi_3_2.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_3.jpg', src: 'img/portfolio_maxi_3_3.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_3.jpg', src: 'img/portfolio_maxi_3_4.jpg', width: 230, height: 250 }
+    ]
+  },
+  ostltd: {
+    organization: 'OSTLtd Company',
+    links: [
+      {
+        href: 'https://ostltd.kz',
+        label: 'ostltd.kz'
+      },
+      {
+        href: 'https://ost.webcreator.top',
+        label: 'ost.webcreator.top'
+      }
+    ],
+    role: 'Full cycle - from a prototype to a functioning website.',
+    technologies: 'Figma, HTML5, CSS3, JS, PHP, SASS, Gulp',
+    description: 'This site for a company that is engaged in wholesale and retail sale of materials for the creation of advertising structures. The main priority is bright colors, modern design, easy navigation, structured catalog, feedback.',
+    images: [
+      { href: 'img/portfolio_full_1.jpg', src: 'img/portfolio_maxi_1.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_1.jpg', src: 'img/portfolio_maxi_2.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_1.jpg', src: 'img/portfolio_maxi_3.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_1.jpg', src: 'img/portfolio_maxi_4.jpg', width: 230, height: 250 }
+    ]
+  },
+  rapid: {
+    organization: 'Rapid Company',
+    links: [
+      {
+        href: 'https://rapidcard.webcreator.top',
+        label: 'rapidcards.webcreator.top'
+      }
+    ],
+    role: 'Frontend development',
+    technologies: 'Figma, HTML5, CSS3, JS, SASS, Gulp, JS Libraries',
+    description: 'This interface was created for a startup that plans to organize a social network. The peculiarity of this social network is the exchange of business cards and the establishment of contacts between businessmen with a modern interface. This interface is adaptive on all types of devices and has form validation.',
+    images: [
+      { href: 'img/portfolio_full_4.jpg', src: 'img/portfolio_maxi_4_1.jpg', width: 230, height: 200 },
+      { href: 'img/portfolio_full_4.jpg', src: 'img/portfolio_maxi_4_2.jpg', width: 230, height: 200 },
+      { href: 'img/portfolio_full_4.jpg', src: 'img/portfolio_maxi_4_3.jpg', width: 230, height: 200 },
+      { href: 'img/portfolio_full_4.jpg', src: 'img/portfolio_maxi_4_4.jpg', width: 230, height: 200 }
+    ]
+  },
+  ae: {
+    organization: 'American Education Company',
+    links: [
+      {
+        href: 'https://volkov85.github.io/american-education/',
+        label: 'volkov85.github.io/american-education'
+      }
+    ],
+    role: 'Full cycle - from a prototype to a functioning website.',
+    technologies: 'Photoshop, HTML5, CSS3, JS, PHP',
+    description: 'This site was developed for American Education. The site has sections about US universities, summer camps for children, teaching English. The site has a feedback form. There is support for the mobile version.',
+    images: [
+      { href: 'img/portfolio_full_5.jpg', src: 'img/portfolio_maxi_5_1.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_5.jpg', src: 'img/portfolio_maxi_5_2.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_5.jpg', src: 'img/portfolio_maxi_5_3.jpg', width: 230, height: 227 },
+      { href: 'img/portfolio_full_5.jpg', src: 'img/portfolio_maxi_5_4.jpg', width: 230, height: 250 }
+    ]
+  },
+  book: {
+    organization: 'Keksobooking educational project',
+    links: [
+      {
+        href: 'https://volkov85.github.io/693011-keksobooking-20/',
+        label: 'volkov85.github.io/693011-keksobooking-20'
+      }
+    ],
+    role: 'JavaScript development',
+    technologies: 'Vanilla JavaScript',
+    description: 'Training project - service for placing and viewing ads "Keksobooking". The service provides users with the ability to post ads about their real estate or view already posted ads. The JavaScript code that implements different functionality of the service is split into corresponding independent modules (IIFEs are used). Sending and receiving data from the server is implemented using AJAX (interaction with the server without page reloading).',
+    images: [
+      { href: 'img/portfolio_full_6.jpg', src: 'img/portfolio_maxi_6_1.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_6.jpg', src: 'img/portfolio_maxi_6_1.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_6.jpg', src: 'img/portfolio_maxi_6_1.jpg', width: 230, height: 250 },
+      { href: 'img/portfolio_full_6.jpg', src: 'img/portfolio_maxi_6_1.jpg', width: 230, height: 250 }
+    ]
+  }
+};
+
+const particlesConfig = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: '#de9654'
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000'
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: 'img/github.svg',
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#855555',
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: 'window',
+    events: {
+      onhover: {
+        enable: false,
+        mode: 'repulse'
+      },
+      onclick: {
+        enable: false,
+        mode: 'push'
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3
+      },
+      repulse: {
+        distance: 100,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+};
+
+const portfolioModal = document.querySelector('#portfolio-modal');
+const portfolioImages = document.querySelector('#portfolio-modal-images');
+const portfolioTitle = document.querySelector('#portfolio-modal-title');
+const portfolioLinks = document.querySelector('#portfolio-modal-links');
+const portfolioRole = document.querySelector('#portfolio-modal-role');
+const portfolioTechnologies = document.querySelector('#portfolio-modal-technologies');
+const portfolioDescription = document.querySelector('#portfolio-modal-description');
+const portfolioButtons = document.querySelectorAll('[data-portfolio]');
+const portfolioCloseButton = document.querySelector('.portfolio__button-close');
 const menuOpener = document.querySelector('.header__menu-toggle');
 const menuCloser = document.querySelector('.header__toggle--close');
 const menuMobile = document.querySelector('.menu-mobile');
-const alertClose = document.querySelector('.alert__close');
 const mainNavItemLink = document.querySelectorAll('.main-nav__item-link');
 const copyrightDate = document.querySelector('#copyright');
+const particlesRoot = document.querySelector('#particles-js');
 
-/**
- * Set current year into copyright
- */
-let currentDate = new Date();
-currentDate = currentDate.getFullYear();
-copyrightDate.textContent = `©${currentDate} Artyom Volkov. All rights reserved.`;
+const updateCopyright = () => {
+  if (!copyrightDate) {
+    return;
+  }
 
-/**
- * Animate value from 0 to value
- * @param {Object} elmnt - counter element
- * @param {number} value - final counter value
- * @param {number} duration - animation duration
- */
-const animateValue = (elmnt, value, duration) =>  {
-  let current = 0;
-  let increment = value > 0 ? 1 : -1;
-  let stepTime = Math.abs(Math.floor(duration / value));
-  let timer = setInterval(() => {
-    current += increment;
-    elmnt.textContent = current;
-
-    if (current === value) {
-      clearInterval(timer);
-    }
-  }, stepTime);
+  const currentDate = new Date().getFullYear();
+  copyrightDate.textContent = `©${currentDate} Artyom Volkov. All rights reserved.`;
 };
 
-/**
- * Smooth scrolling script
- */
-mainNavItemLink.forEach(item => {
-  item.addEventListener('click', (evt) => {
-    if (evt.target.hash !== "") {
+const closeMenu = () => {
+  if (!menuMobile || !menuOpener) {
+    return;
+  }
+
+  menuMobile.classList.add('menu-mobile--closed');
+  menuMobile.setAttribute('aria-hidden', 'true');
+  menuOpener.setAttribute('aria-expanded', 'false');
+};
+
+const openMenu = () => {
+  if (!menuMobile || !menuOpener) {
+    return;
+  }
+
+  menuMobile.classList.remove('menu-mobile--closed');
+  menuMobile.setAttribute('aria-hidden', 'false');
+  menuOpener.setAttribute('aria-expanded', 'true');
+};
+
+const createPortfolioLink = (link, index) => {
+  const anchor = document.createElement('a');
+  anchor.className = 'portfolio__card-link';
+  anchor.href = link.href;
+  anchor.target = '_blank';
+  anchor.rel = 'noopener noreferrer';
+  anchor.textContent = link.label;
+
+  if (index > 0) {
+    const separator = document.createTextNode(', ');
+    portfolioLinks.appendChild(separator);
+  }
+
+  portfolioLinks.appendChild(anchor);
+};
+
+const renderPortfolioImages = (images) => {
+  portfolioImages.innerHTML = '';
+
+  images.forEach((image) => {
+    const anchor = document.createElement('a');
+    const img = document.createElement('img');
+
+    anchor.href = image.href;
+    anchor.target = '_blank';
+    anchor.rel = 'noopener noreferrer';
+
+    img.src = image.src;
+    img.alt = `${portfolioTitle.textContent} image`;
+    img.width = image.width;
+    img.height = image.height;
+    img.loading = 'lazy';
+    img.decoding = 'async';
+
+    anchor.appendChild(img);
+    portfolioImages.appendChild(anchor);
+  });
+};
+
+const openPortfolioModal = (portfolioKey) => {
+  const currentPortfolio = portfolioData[portfolioKey];
+
+  if (!currentPortfolio || !portfolioModal) {
+    return;
+  }
+
+  portfolioTitle.textContent = currentPortfolio.organization;
+  portfolioRole.textContent = currentPortfolio.role;
+  portfolioTechnologies.textContent = currentPortfolio.technologies;
+  portfolioDescription.textContent = currentPortfolio.description;
+  portfolioLinks.textContent = '';
+
+  currentPortfolio.links.forEach((link, index) => {
+    createPortfolioLink(link, index);
+  });
+
+  renderPortfolioImages(currentPortfolio.images);
+
+  portfolioModal.classList.add('portfolio__darker--opened');
+  document.body.style.overflow = 'hidden';
+};
+
+const closePortfolioModal = () => {
+  if (!portfolioModal) {
+    return;
+  }
+
+  portfolioModal.classList.remove('portfolio__darker--opened');
+  document.body.style.overflow = '';
+};
+
+const initSmoothScroll = () => {
+  mainNavItemLink.forEach((item) => {
+    item.addEventListener('click', (evt) => {
+      const hash = evt.currentTarget.hash;
+
+      if (!hash) {
+        closeMenu();
+        return;
+      }
+
+      const section = document.querySelector(hash);
+
+      if (!section) {
+        return;
+      }
+
       evt.preventDefault();
-      let hash = evt.target.hash;
-      document.querySelector(hash).scrollIntoView({
+      section.scrollIntoView({
         behavior: 'smooth',
         block: 'start'
       });
+      closeMenu();
+    });
+  });
+};
+
+const initPortfolioModal = () => {
+  if (!portfolioModal || !portfolioCloseButton) {
+    return;
+  }
+
+  portfolioButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      openPortfolioModal(button.dataset.portfolio);
+    });
+  });
+
+  portfolioCloseButton.addEventListener('click', closePortfolioModal);
+
+  portfolioModal.addEventListener('click', (evt) => {
+    if (evt.target === portfolioModal) {
+      closePortfolioModal();
     }
   });
-});
 
-
-/**
- * PHPmailer popup alert hiding
- */
-if (alertClose) {
-  alertClose.addEventListener('click', () => {
-    const alertPopup = document.querySelector('.alert-success');
-    alertPopup.style = 'visibility: hidden;';
-  });
-}
-
-/**
- * Portfolio popup closing event
- */
-portfolioButtonClose.forEach(item => {
-  item.addEventListener('click', evt => {
-    evt.preventDefault();
-    portfolioDarker.forEach(item => item.classList.remove('portfolio__darker--opened'));
-  });
-});
-
-/**
- * Portfolio popup open event
- */
-portfolioCards.forEach(item => {
-  item.addEventListener('click', () => {
-    const openerClass = 'portfolio__darker--opened';
-    const containClass = 'portfolio__cards-button';
-
-    switch (true) {
-      case item.classList.contains(`${containClass}--ostltd`):
-        portfolioDarkerOstltd.classList.add(openerClass);
-        break;
-      case item.classList.contains(`${containClass}--nda`):
-        portfolioDarkerNda.classList.add(openerClass);
-        break;
-      case item.classList.contains(`${containClass}--fantshop`):
-        portfolioDarkerFant.classList.add(openerClass);
-        break;
-      case item.classList.contains(`${containClass}--rapid`):
-        portfolioDarkerRapid.classList.add(openerClass);
-        break;
-      case item.classList.contains(`${containClass}--ae`):
-        portfolioDarkerAe.classList.add(openerClass);
-        break;
-      case item.classList.contains(`${containClass}--book`):
-        portfolioDarkerBook.classList.add(openerClass);
-        break;
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      closePortfolioModal();
+      closeMenu();
     }
   });
-});
+};
 
-/**
- * Mobile menu toggler
- */
-menuOpener.addEventListener('click', () => {
-  menuMobile.classList.remove('menu-mobile--closed');
-});
-menuCloser.addEventListener('click', () => {
-  menuMobile.classList.add('menu-mobile--closed');
-});
+const initMenu = () => {
+  if (!menuOpener || !menuCloser) {
+    return;
+  }
+
+  menuOpener.addEventListener('click', openMenu);
+  menuCloser.addEventListener('click', closeMenu);
+};
+
+const loadParticles = () => {
+  if (!particlesRoot) {
+    return;
+  }
+
+  const prefersReducedMotion = window.matchMedia &&
+    window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
+  if (prefersReducedMotion || window.innerWidth <= 768) {
+    return;
+  }
+
+  const initParticles = () => {
+    const particlesScript = document.createElement('script');
+    particlesScript.src = 'js/particles.min.js';
+    particlesScript.async = true;
+    particlesScript.onload = () => {
+      if (window.particlesJS) {
+        window.particlesJS('particles-js', particlesConfig);
+      }
+    };
+    document.body.appendChild(particlesScript);
+  };
+
+  if ('requestIdleCallback' in window) {
+    window.requestIdleCallback(initParticles, {timeout: 1500});
+    return;
+  }
+
+  window.setTimeout(initParticles, 400);
+};
+
+updateCopyright();
+initSmoothScroll();
+initPortfolioModal();
+initMenu();
+window.addEventListener('load', loadParticles, {once: true});
